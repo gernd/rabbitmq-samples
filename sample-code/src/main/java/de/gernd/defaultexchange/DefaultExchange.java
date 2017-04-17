6ea4e67c979f091e8054dcf8cff883c477c63055
Gernot Pointner <gernot.pointner@googlemail.com>
2017-04-17 15:33:10 +0200

    Implement periodic message sending

---
+++ sample-code/src/main/java/de/gernd/defaultexchange/DefaultExchange.java
@@ -0,0 +1,52 @@
+package de.gernd.defaultexchange;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class DefaultExchange {
+
+    public static final String QUEUE_NAME = "defaultExchangeQueue";
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        System.out.println("Continously sending messages until ENTER is pressed");
+
+        // set up sending connection to rabbitmq broker
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection rabbitMqConnection = factory.newConnection();
+
+        // declare queue
+        Channel channel = rabbitMqConnection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        // start sending random messages
+        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(new RandomMessageSender
+                (channel), 0, 5, TimeUnit.SECONDS);
+
+        // set up message consumption for test queue
+        Consumer messageConsumer = new DefaultConsumer(channel) {
+
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                                       AMQP.BasicProperties properties, byte[] body)
+                    throws IOException {
+                String receivedMessage = new String(body, "UTF-8");
+                System.out.println("Got message: " + receivedMessage);
+            }
+
+        };
+        channel.basicConsume(QUEUE_NAME, true, messageConsumer);
+
+        // wait until user presses ENTER
+        System.in.read();
+
+        // close send channel and connection
+        System.out.println("Shutting down");
+        channel.close();
+        rabbitMqConnection.close();
+    }
+}
---
+++ sample-code/src/main/java/de/gernd/defaultexchange/RandomMessageSender.java
@@ -0,0 +1,41 @@
+package de.gernd.defaultexchange;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.Random;
+
+import static de.gernd.defaultexchange.DefaultExchange.QUEUE_NAME;
+
+public class RandomMessageSender implements Runnable {
+
+    private final Channel channel;
+
+    private final Random random = new Random();
+
+    public RandomMessageSender(Channel channel) {
+        this.channel = channel;
+    }
+
+    public void run() {
+        Long randomVal = random.nextLong();
+        final String message = new StringBuilder()
+                .append("Message[")
+                .append(randomVal)
+                .append("]")
+                .toString();
+        System.out.println("Sending message " + message);
+        try {
+            // send message
+            // the message is sent using the broker with no exchange specified
+            // -> default exchange is used
+            // the routing key is the name of the queue that has been created before
+            // the default queue has a special poperty: every queue, that is created,
+            // is automatically bound to the default exchange with its name as the routing
+            // key.
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
